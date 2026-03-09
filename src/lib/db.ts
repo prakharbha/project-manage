50942b0ab7b5d4dc2850f3da866f997c906c54e9
Prakhar <prakhar@Prakhars-MacBook-Pro.local>
2026-03-10 00:48:03 +0530

    fix: set explicit prisma generate output to src/generated/prisma for Vercel compatibility

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from '@prisma/client'
-
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
